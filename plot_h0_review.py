09926ad21ae9afbca615409d0a20d1b583ffc5a4
Pablo-Lemos <plemos91@gmail.com>
2021-08-16 17:45:06 +0200

    Improved example and plot for H0 review

--- plot_h0_review.py
+++ plot_h0_review.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     points_file = 'csv_files/h0_review.csv'
     plotter.plotter(points_file,
                     xlabel=r'$ H_0 \ [\mathrm{km \ s^{-1} \ Mpc^{-1}]}$',
-                    save=False,
+                    save=True,
                     out_folder='plots',
                     plot_name='h0_review',
                     xmargin=0.12)
